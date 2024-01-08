641e43f3a6e07be392d37497a1004337cd8d8490
vymvn <vymvn6@gmail.com>
2024-01-08 22:16:18 +0400

    somewhat working

--- main.go
+++ main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
 	"net"
 	"os"
 )
 
 
-func getSize(conn net.Conn) string {
+type canvasSize struct {
+    width  int
+    height int
+}
 
-    size := make([]byte, 1024)
+func getSize(conn net.Conn) *canvasSize {
+
+    var size canvasSize
     conn.Write([]byte("SIZE\n"))
-    _, err := conn.Read(size);
+    reply, err := bufio.NewReader(conn).ReadString('\n')
     if err != nil {
         fmt.Println("Could not get size: ", err)
-        return ""
+        return nil
     }
-    return string(size)
+
+    fmt.Sscanf(reply, "SIZE %d %d", &size.width, &size.height)
+    return &size
 }
 
 func writePixel(x, y, r, g, b, a int, conn net.Conn) {
@@ -30,11 +41,42 @@ func writePixel(x, y, r, g, b, a int, conn net.Conn) {
 	conn.Write([]byte(cmd))
 }
 
+func drawRect(x, y, w, h, r, g, b int, conn net.Conn) {
+	for i := x; i < x+w; i++ {
+		for j := y; j < y+h; j++ {
+			writePixel(i, j, r, g, b, 255, conn)
+		}
+	}
+}
+
+func drawImage(path string, startX int, startY int, conn net.Conn) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return err
+	}
+
+	bounds := img.Bounds()
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			r, g, b, a := img.At(x, y).RGBA()
+			writePixel(x + startX, y + startY, int(r>>8), int(g>>8), int(b>>8), int(a>>8), conn)
+		}
+	}
+
+	return nil
+}
 
 func main() {
 
-    var host *string = flag.String("host", "", "The PixelFlut server host ip or domain.")
-    var port *string = flag.String("port", "", "The port of the PixelFlut server.")
+    var host      *string = flag.String("host", "", "The PixelFlut server host ip or domain.")
+    var port      *string = flag.String("port", "", "The port of the PixelFlut server.")
+    var imagePath *string = flag.String("image", "", "The path to the image to draw.")
 
     required := []string{"host", "port"}
     flag.Parse()
@@ -51,9 +93,11 @@ func main() {
     connString := fmt.Sprintf("%s:%s", *host, *port)
     conn, err := net.Dial("tcp", connString)
     if err != nil {
-        fmt.Println("Could not connect to \"" + connString + "\":\n\t", err)
+        fmt.Fprintln(os.Stderr, "Could not connect to \"" + connString + "\":\n\t", err)
         os.Exit(1)
     }
     defer conn.Close()
+
+    drawImage(*imagePath, 100, 100, conn)
 
 }
