036912a8175322213cbbebaa9bff8955c5483ac4
vymvn <vymvn6@gmail.com>
2024-01-10 03:22:26 +0400

    text rendering from the hood

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/vymvn/goflut
 
 go 1.21.5
 
-require golang.org/x/image v0.15.0
-
-require golang.org/x/text v0.14.0 // indirect
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	golang.org/x/image v0.15.0
+)
--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-    "bufio"
-    "flag"
-    "fmt"
-    "image"
-    _ "image/jpeg"
-    _ "image/png"
-    "math"
-    "net"
-    "os"
-    "sync"
-    // "time"
+	"bufio"
+	"flag"
+	"fmt"
+	"image"
+	// "image/color"
+	// "image/draw"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"math"
+	"net"
+	"os"
+	"sync"
+
+	"github.com/golang/freetype"
+	"golang.org/x/image/font"
+	// "time"
 )
 
 
@@ -29,6 +35,12 @@ type chunk struct {
 
 
 var (
+
+    host      *string = flag.String("host", "", "The PixelFlut server host ip or domain.")
+    port      *string = flag.String("port", "", "The port of the PixelFlut server.")
+    imagePath *string = flag.String("image", "", "The path to the image to draw.")
+    threads      *int = flag.Int("threads", 1, "Number of threads to use.")
+
     canvasSize size
 )
 
@@ -192,6 +204,55 @@ func drawImage(path string, startX, startY int, threads int, size float64, conn 
     return nil
 }
 
+func drawText(text string, startX, startY int, size float64, conn net.Conn) {
+
+    fontBytes, err := os.ReadFile("fonts/Lato-Regular.ttf")
+    if err != nil {
+        log.Println(err)
+        return
+    }
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Initialize the context.
+	// fg, bg := image.Black, image.White
+	fg := image.Black
+	rgba := image.NewRGBA(image.Rect(0, 0, 800, 200))
+	// draw.Draw(rgba, rgba.Bounds(), bg, image.Point{}, draw.Src)
+	c := freetype.NewContext()
+	c.SetDPI(300)
+	c.SetFont(f)
+	c.SetFontSize(size)
+	c.SetClip(rgba.Bounds())
+	c.SetDst(rgba)
+	c.SetSrc(fg)
+    c.SetHinting(font.HintingNone)
+    // c.SetHinting(font.HintingFull)
+
+    pt := freetype.Pt(10, 5 +int(c.PointToFixed(size) >> 6))
+
+    if _, err := c.DrawString(text, pt); err != nil {
+        log.Println(err)
+        return
+    }
+
+    bounds := rgba.Bounds()
+    width  := bounds.Max.X
+    height := bounds.Max.Y
+
+    // processImage(rgba, -1, -1, 1, 1, conn)
+    for x := 0; x < width; x++ {
+        for y := 0; y < height; y++ {
+            r, g, b, a := rgba.At(x, y).RGBA()
+            writePixel(x + startX, y + startY, int(r>>8), int(g>>8), int(b>>8), int(a>>8), conn)
+        }
+    }
+
+}
+
 func wipeCanvas(conn net.Conn) {
     drawRect(0, 0, canvasSize.width, canvasSize.height, 50, 50, 50, conn)
 }
@@ -201,11 +262,6 @@ func applyBackground(r, g, b int, conn net.Conn) {
 }
 
 func main() {
-
-    var host      *string = flag.String("host", "", "The PixelFlut server host ip or domain.")
-    var port      *string = flag.String("port", "", "The port of the PixelFlut server.")
-    var imagePath *string = flag.String("image", "", "The path to the image to draw.")
-    var threads      *int = flag.Int("threads", 1, "Number of threads to use.")
 
     required := []string{"host", "port"}
     flag.Parse()
@@ -233,10 +289,10 @@ func main() {
     defer conn.Close()
 
     getCanvasSize(conn)
+    wipeCanvas(conn)
+    drawText("text rendering from the hood", 100, 100, 12, conn)
 
-    // wipeCanvas(conn)
-
-    bouncingImage(0, 0, 50, 70, *imagePath, 0.2, conn, *threads)
+    // bouncingImage(0, 0, 50, 70, *imagePath, 0.2, conn, *threads)
 
     // err = drawImage(*imagePath, -1, -1, *threads, 0.5, conn)
     // if err != nil {
